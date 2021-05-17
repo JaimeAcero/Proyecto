e7b35513ed9e65257897b80590ec4d354f5e1f16
JaimeAcero <est.jaime.acero@unimilitar.edu.co>
2021-05-17 11:36:20 -0500

    Initial commit

---
+++ funcion.js
@@ -0,0 +1,11 @@
+function cambio(){
+document.getElementById("userpic").src= "https://www.ecestaticos.com/image/clipping/79776773aab795837282c7d4947abaf7/por-que-nos-parece-que-los-perros-sonrien-una-historia-de-30-000-anos.jpg";
+document.getElementById("Entrar").hidden = true;
+document.getElementById("Salir").hidden = false;
+}
+
+function cambio2(){
+document.getElementById("userpic").src= "https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918647/user.svg";
+document.getElementById("Entrar").hidden = false;
+document.getElementById("Salir").hidden = true;
+}
